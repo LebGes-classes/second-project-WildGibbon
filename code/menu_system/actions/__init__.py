1d172313067708212d9b957e94c2079a04a60a7b
WildGibbon <chelovekebola@mail.ru>
2026-05-22 23:57:24 +0300

    docstrings added

---
+++ code/menu_system/actions/__init__.py
@@ -0,0 +1,11 @@
+from .IAction import IAction
+from .MockAction import MockAction
+from .DelegateAction import DelegateAction
+from .OpenMenuAction import OpenMenuAction
+
+__all__ = [
+    'IAction',
+    'MockAction',
+    'DelegateAction',
+    'OpenMenuAction'
+]
